91c4aadd70fec8c5e1caeb64e519ab0fd9dbab2b
RKSona <fuseon6205@gmail.com>
2023-08-07 20:14:30 +0100

    Regions uploaded
    5 quid says this gets changed at least two times before we upload the base arch

---
+++ worlds/celeste/Regions.py
@@ -0,0 +1,31 @@
+from BaseClasses import MultiWorld
+
+
+def link_celeste_areas(world: MultiWorld, player: int):
+    for (exit, region) in mandatory_connections:
+        world.get_entrance(exit, player).connect(world.get_region(region, player))
+        
+
+# (Region name, list of exits)
+celeste_regions = [
+    ("Menu", ["City Entrance"]),
+    ("Forsaken City", ["Site Entrance"]),
+    ("Old Site", ["Resort Entrance"]),
+    ("Celestial Resort", ["Ridge Entrance"]),
+    ("Golden Ridge", ["Temple Entrance"]),
+    ("Mirror Temple", ["Reflect Entrance"]),
+    ("Reflections", ["Summit Entrance"]),
+    ("Summit", []),
+]
+
+
+# (Entrance, region pointed to)
+mandatory_connections = [
+    ("City Entrance", "Forsaken City"),
+    ("Site Entrance", "Old Site"),
+    ("Resort Entrance", "Celestial Resort")
+    ("Ridge Entrance", "Golden Ridge"),
+    ("Temple Entrance", "Mirror Temple"),
+    ("Reflect Entrance", "Reflections")
+    ("Summit Entrance", "Summit")
+]
