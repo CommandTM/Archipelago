b42bb9252b7d6ea4a9e9b0c0228d6b9da2689313
CommandTM <117131509+CommandTM@users.noreply.github.com>
2023-08-24 07:44:28 -0400

    Made __init__.py’s webworld original
    If this isn't original, you can sue me for 5 quid

--- worlds/celeste/__init__.py
+++ worlds/celeste/__init__.py
@@ -4,7 +4,7 @@ from worlds.AutoWorld import World, WebWorld
 class CelesteWeb(WebWorld):
     tutorials = [Tutorial(
         "Multiworld Setup Tutorial",
-        "A guide to setting up the Archipelago Celeste game on your computer.",
+        "A guide to making a computer play Celeste that just so happens to have Archipelago combatibility.",
         "English",
         "setup_en.md",
         "setup/en",
