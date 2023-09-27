5b9b26b300bfd8af91264fd74fc2489b479b5300
colin-every <commandnoobmc23@gmail.com>
2023-09-27 10:17:23 -0400

    __init__ and Info page

---
+++ worlds/bk_picross/__init__.py
@@ -0,0 +1,33 @@
+from typings import dict
+
+from BaseClasses import Tutorial
+from ..AutoWorld import WebWorld, World
+
+class BK_PicrossWebWord(WebWorld):
+    settings_page = "games/Picross/info/en"
+    theme = "partyTime"
+    tutorials = [
+        Tutorial(
+            tutorial_name='Setup Guide',
+            description='A guide to playing BK Picross',
+            language='English',
+            file_name='setup_en.md',
+            link='setup/en',
+            authors=['CommandTM']
+        )
+    ]
+
+class BK_PicrossWorld(World):
+    """
+    Play Picross while BKed to gain useful hints
+    """
+    game = "Picross"
+    web = BK_PudokuWebWorld()
+    data_version = 1
+
+    item_name_to_id: Dict[str, int] = {}
+    location_name_to_id: Dict[str, int] = {}
+
+    @classmethod
+    def stage_assert_generate(cls, multiworld):
+        raise Exception("BK Picross is not generated into worlds, but connects to pre-existing worlds")
---
+++ worlds/bk_picross/docs/en_Picross.md
@@ -0,0 +1,13 @@
+# BK Picross
+
+## What is this game?
+
+BK Picross is an atypical Archipelago game. It can be used to connect to any existing Multiworld, and is used to unlock hints for your game.
+
+## What kind of hints are unlocked?
+
+After completing a picross puzzle, you'll get 1 hint from an unchecked location in the slot you are connected to.
+
+## Where is the settings page?
+
+Due to the nature of BK Picross, there is no settings page; instead the client can connect to any existing slot in a Multiworld.
