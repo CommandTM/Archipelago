c5c3383e82a4d47478fa35d9543f76cfd502497c
colin-every <commandnoobmc23@gmail.com>
2023-11-13 09:29:50 -0500

    Basic Item System Maybe

---
+++ worlds/haiku/data/LocationDict.py
@@ -0,0 +1,11 @@
+abilites = {
+    "Electro-Magnetism": "Abandoned Wastes",
+    "Body Modifier": "Factory Facility",
+    "Bulblet": "Central Core",
+    "Heat Treatment": "Central Core",
+    "Sealant Treatment": "Central Core",
+    "Jump Boosters": "Water Ducts",
+    "String And Hook": "Central Core",
+    "Space Disruptor": "Forgotten Ruins",
+    "Power Bomb": "Central Core"
+}
